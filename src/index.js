31ff324af1406796b51d5e552ddce6475e9a6c6c
Talissa <liayai@ya.ru>
2024-08-11 18:46:16 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  if (str.length % 2 !== 0) {
+    return false;
+  };
+  let newStr = '';
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    bracketsConfig[i] = bracketsConfig[i][0] + bracketsConfig[i][1];
+  };
+  while (str !== newStr) {
+    newStr = str;
+    for (let j = 0; j < bracketsConfig.length; j++) {
+    str = str.replace(bracketsConfig[j],'');
+    };
+  };
+  
+  return (newStr === '');
+};
